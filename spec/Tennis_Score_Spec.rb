473b89d986d7ee071fdd5f2c129b8a655fe3d4c1
alexanderwjrussell <awjrussell@live.co.uk>
2016-05-18 09:31:17 +0100

    Fourth test written and failing
    Replacing the 0 in 30 with "Love". Interesting...

--- spec/Tennis_Score_Spec.rb
+++ spec/Tennis_Score_Spec.rb
@@ -18,4 +18,8 @@ it "Returns 15-15 when score is 15-15" do
   expect(@Tennis_Score.score("15", "15")).to eq("15-15")
 end
 
+it "Returns 30-15 when score is 30-15" do
+  expect(@Tennis_Score.score("30", "15")).to eq("30-15")
+end
+
 end
