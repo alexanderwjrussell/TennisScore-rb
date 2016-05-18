173720b150c79732cf95ef59bbe65ffae9876d6c
alexanderwjrussell <awjrussell@live.co.uk>
2016-05-18 09:19:45 +0100

    Second test now working

--- Tennis_Score.rb
+++ Tennis_Score.rb
@@ -1,9 +1,15 @@
 class Tennis_Score
 
 def score(value1, value2)
-  value1 = "Love"
+  if value1 == "0"
+    value1 = "Love"
+  else
+    value1 = value1
+    value1
+  end
+
   value2 = "Love"
-  value1 + "-" + value2 
+  value1 + "-" + value2
 end
 
 end
