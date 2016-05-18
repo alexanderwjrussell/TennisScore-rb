d411d16a3d517bb57e449e70fcf0c8b00bab1dc3
alexanderwjrussell <awjrussell@live.co.uk>
2016-05-18 09:10:26 +0100

    First test initialised and failing

--- spec/Tennis_Score_Spec.rb
+++ spec/Tennis_Score_Spec.rb
@@ -5,3 +5,9 @@ describe Tennis_Score do
 before :each do
   @Tennis_Score = Tennis_Score.new
 end
+
+it "Returns Love-Love when score is 0-0" do
+  expect(@Tennis_Score.add("0", "0")).to eq("Love-Love")
+end
+
+end
